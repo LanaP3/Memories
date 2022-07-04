0b9ad1317e9215161fc73d91020ccd189e87d69e
Lana Prijon <lana.prijon@gmail.com>
2022-07-04 23:24:48 +0200

    popravki

--- model.py
+++ model.py
@@ -1,5 +1,8 @@
+from datetime import date
+
 class Album:
-    def __init__(self, ime, images, public=False):
+    def __init__(self, date, ime, images, public=False):
+        self.date = date.today()
         self.ime = ime
         self.images = images
         self.public = public
@@ -10,27 +13,42 @@ class Album:
     def add_image(self):
         pass
 
-    def change_access(self):
-        pass
+    def make_public(self):
+        self.public = True
 
-    
+    def make_private(self):
+        self.public = False
+
+
 class Image:
-    def __init__(self, ime, ranking, comments):
+    def __init__(self, date, ime, likes, dislikes, comments):
+        self.date = date.today()
         self.ime = ime
-        self.ranking = ranking
+        self.likes = likes
+        self.dislikes = dislikes
         self.comments = comments
 
     def download(self):
         pass
 
-    def add_comment(self):
-        pass
+    def add_comment(self, comment):
+        self.comments.append(comment)
 
-    def rate(self):
-        pass
+    def like(self):
+        self.likes += 1
+
+    def dislike(self):
+        self.dislikes += 1
+
+    def rating(self):
+        if self.likes+self.dislikes == 0:
+            return None
+        else:
+            return ((self.likes-self.dislikes)/(self.likes+self.dislikes) * 5) + 5
 
 
-#class Comment:
-#    def __init__(self, text):
-#        self.text = text
+class Comment:
+    def __init__(self, date, comment):
+        self.date = date.today()
+        self.comment = comment
 
