29a183629decec175f4269c81b3322afd44015e7
Lana Prijon <lana.prijon@gmail.com>
2022-07-06 18:42:58 +0200

    prvi osnutek spletnega vmesnika

--- src/main.py
+++ src/main.py
@@ -3,4 +3,27 @@ import os
 import sys
 from bottle import get, post, request
 from datetime import datetime
+
+from click import password_option
 from model import *
+
+bottle.TEMPLATE_PATH.insert(0, os.path.join(os.getcwd(), "view"))
+
+@bottle.get("/")
+def login_page():
+    return bottle.template("login.tpl", error=None)
+
+@bottle.post("/")
+def login_post():
+    username = bottle.request.forms.getunicode("Username")
+    password = bottle.request.forms.getunicode("Password")
+    if username and password:
+        bottle.redirect("/home_page/")
+    else:
+        return bottle.template("login.tpl", error="Wrong password")
+
+@bottle.get("/home_page/")
+def home_page():
+    return bottle.template("home_page.tpl")
+
+bottle.run(reloader=True)
